5ff94e1d48ce56dec0a3e65dbd9b0a18705694c7
elif-demir <elifdemir143@hotmail.com>
2019-09-02 16:22:20 +0300

    Training

---
+++ certification/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Certification',
+    'summary': "Defines certification for different purposes.",
+    'author': "Eficent, Odoo Community Association (OCA)",
+    'website': "https://github.com/elif-demir",
+    'category': 'Certification Management',
+    'verison': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': ['security/ir.model.access.csv',
+             'views/certification_view.xml',
+             'views/standard_view.xml'
+             ],
+    'development_status': 'Beta',
+    'maintainers': ['certificent'],
+}

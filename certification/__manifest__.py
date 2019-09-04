85773ae6091844e597da06a8a346702d5e346373
elif-demir <elifdemir143@hotmail.com>
2019-09-04 14:08:21 +0300

    Trainig: Employee ID Card Module

--- certification/__manifest__.py
+++ certification/__manifest__.py
@@ -7,10 +7,13 @@
     'verison': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': ['base'],
-    'data': ['security/ir.model.access.csv',
+    'data': ['security/certification_security.xml',
+             'security/ir.model.access.csv',
              'views/certification_view.xml',
+             'views/res_partner_view.xml',
              'views/standard_view.xml'
              ],
+    'demo': ['data/certification_data.xml'],
     'development_status': 'Beta',
     'maintainers': ['certificent'],
 }
